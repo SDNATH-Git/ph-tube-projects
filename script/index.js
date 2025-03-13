432d4b2bc8abe6b51fb098847d54cef07822a72b
SDNATH-Git <sd.nath1952@gmail.com>
2025-03-14 00:56:52 +0600

    added by card

--- script/index.js
+++ script/index.js
@@ -23,3 +23,61 @@ function display(categories){
 }
 
 loadcategoris()
+
+/*
+* video card section API call 
+*/
+function loadVideoCard(){
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then((res)=>res.json())
+    .then((data)=>displayVideo(data.videos))
+
+}
+const displayVideo = (videos) =>{
+
+    const videoCard = document.getElementById('videoCard');
+
+    videos.forEach((video)=>{
+        // console.log(video);
+        const divCard = document.createElement('div');
+        divCard.innerHTML=`
+         <div class="card bg-base-100 ">
+            <figure class="relative">
+                <img class="w-full h-[150px] object-cover" src="${video.thumbnail}" />
+
+                <span class="absolute mt-28 ml-36 bg-black text-white px-2 py-1 text-sm rounded-md">3hrs 56 min
+                    ago</span>
+            </figure>
+            
+            <div class="py-3 flex gap-3">
+                <div class="profile">
+                    <div class="avatar w-12">
+                        <div class="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
+                            <img src="${video.authors[0].profile_picture}" />
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class=" text ">
+                    <h1 class="font-bold mb-2">${video.title}</h1>
+                    <p class="text-sm font-medium text-gray-500 flex gap-1 mb-1">
+                        ${video.authors[0].profile_name}
+                        <img class="w-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt="">
+                    </p>
+
+                    <p class="text-sm font-medium text-gray-500">${video.others.views}</p>
+
+                </div>
+            </div>
+        </div>
+        
+        `
+        videoCard.appendChild(divCard);
+    
+    })
+           
+    
+}
+
+loadVideoCard();
